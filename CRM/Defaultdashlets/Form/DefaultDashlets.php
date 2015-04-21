6870970c80d4862f297e9e72d7ce4e413ad1371c
David Hayes <david@blackbricksoftware.com>
2015-04-21 18:11:01 -0500

    save, load setting data

--- CRM/Defaultdashlets/Form/DefaultDashlets.php
+++ CRM/Defaultdashlets/Form/DefaultDashlets.php
@@ -8,23 +8,23 @@ require_once 'CRM/Core/Form.php';
  * @see http://wiki.civicrm.org/confluence/display/CRMDOC43/QuickForm+Reference
  */
 class CRM_Defaultdashlets_Form_DefaultDashlets extends CRM_Core_Form {
-  function buildQuickForm() {
-
-    //~ // add form elements
-    //~ $this->add(
-      //~ 'select', // field type
-      //~ 'favorite_color', // field name
-      //~ 'Favorite Color', // field label
-      //~ $this->getColorOptions(), // list of options
-      //~ true // is required
-    //~ );
-    //~ $this->addButtons(array(
-      //~ array(
-        //~ 'type' => 'submit',
-        //~ 'name' => ts('Submit'),
-        //~ 'isDefault' => TRUE,
-      //~ ),
-    //~ ));
+	
+	//~ function setDefaultValues() {
+		//~ $defaults = array(
+			//~ 'defaultdashlets' => ,
+		//~ );
+		//~ return $defaults;
+	//~ }
+	
+	function buildQuickForm() {
+		
+	 $this->addButtons(array(
+      array(
+        'type' => 'submit',
+        'name' => ts('Submit'),
+        'isDefault' => TRUE,
+      ),
+    ));
 
 	$groups = civicrm_api3('Group', 'get', array(
 		'option.limit' => 0,
@@ -37,17 +37,42 @@ class CRM_Defaultdashlets_Form_DefaultDashlets extends CRM_Core_Form {
 		'option.limit' => 0,
 	));
 	$this->assign('avalabledashlets', $avalabledashlets['values']);
+	
+	$defaultdashlets = CRM_Core_BAO_Setting::getItem('DefaultDashlets', 'defaultdashlets');
+	$this->assign('defaultdashlets', $defaultdashlets);
  
+	$this->assign('elementNames', $this->getRenderableElementNames());
     parent::buildQuickForm();
   }
 
   function postProcess() {
-    $values = $this->exportValues();
-
-	echo '<pre>',print_r($values),'</pre>'; exit;
+    $values = $this->getSubmitValues();
+    
+	CRM_Core_BAO_Setting::setItem($values['defaultdashlets'], 'DefaultDashlets', 'defaultdashlets');
 
     CRM_Core_Session::setStatus(ts('Saved!'));
     parent::postProcess();
+  }
+  
+  /**
+   * Get the fields/elements defined in this form.
+   *
+   * @return array (string)
+   */
+  function getRenderableElementNames() {
+    // The _elements list includes some items which should not be
+    // auto-rendered in the loop -- such as "qfKey" and "buttons".  These
+    // items don't have labels.  We'll identify renderable by filtering on
+    // the 'label'.
+    $elementNames = array();
+    foreach ($this->_elements as $element) {
+      /** @var HTML_QuickForm_Element $element */
+      $label = $element->getLabel();
+      if (!empty($label)) {
+        $elementNames[] = $element->getName();
+      }
+    }
+    return $elementNames;
   }
 
 }
--- templates/CRM/Defaultdashlets/Form/DefaultDashlets.tpl
+++ templates/CRM/Defaultdashlets/Form/DefaultDashlets.tpl
@@ -7,7 +7,7 @@
 {foreach from=$groups item=group}
 	<h3>{$group.name}</h3>
 	{foreach from=$avalabledashlets item=avalabledashlet}
-		<input type="checkbox" value="1" name="defaultdashlets[{$group.id}][{$avalabledashlet.id}]"> {$avalabledashlet.label} &nbsp;&nbsp;&nbsp;
+		<input type="checkbox" value="1" name="defaultdashlets[{$group.id}][{$avalabledashlet.id}]" {if !empty($defaultdashlets[$group.id][$avalabledashlet.id])}checked{/if}> {$avalabledashlet.label} &nbsp;&nbsp;&nbsp;
 	{/foreach}
 	<br>
 {/foreach}
